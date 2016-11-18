44035a18c2a5dcd5477f41e50758bc6f5d2d478f
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-18 19:38:47 +0800

    Minor refactoring2

--- src/AtmCommandFactory.java
+++ src/AtmCommandFactory.java
@@ -1,18 +1,15 @@
 /**
  * Created by Test on 10/30/2016.
  */
-//TODO: doesn't have to be public - fix all such cases when "public" modifier can be left out
-public class AtmCommandFactory {
+class AtmCommandFactory {
 
     private final MoneyStorage moneyStorage;
 
-    public AtmCommandFactory(MoneyStorage moneyStorage) {
+    AtmCommandFactory(MoneyStorage moneyStorage) {
         this.moneyStorage = moneyStorage;
     }
 
     AtmCommand create(String action) throws AtmStateException {
-
-
         switch (CommandType.getCommandType(action)) {
             case REMAININGS:
                 return new RequestRemainings(moneyStorage);
@@ -20,13 +17,8 @@ public class AtmCommandFactory {
                 return new DepositCommand(moneyStorage);
             case WITHDRAW:
                 return new WithdrawalCommand(moneyStorage);
-            // TODO: get rid of this - the factory should not do system exit - it should just create commands
-            case EXIT:
-                System.exit(0);
             default:
-                //TODO: let's write a normal English phrase - not in capital letters: "Can not find...."
-                // TODO: should throw AtmStateException
-                throw new IllegalArgumentException("CAN NOT FIND A COMMAND FOR THIS ACTION " + action);
+                throw new AtmStateException("Cannot find a command for this action " + action);
 
         }
     }
