491fd16488d07137db93433a9626c22e579860ec
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-18 19:32:33 +0800

    Minor refactoring2

--- src/RequestRemainings.java
+++ src/RequestRemainings.java
@@ -3,11 +3,11 @@ import java.util.Map;
 /**
  * Created by Test on 10/30/2016.
  */
-public class RequestRemainings implements AtmCommand {
+class RequestRemainings implements AtmCommand {
 
     private MoneyStorage moneyStorage;
 
-    public RequestRemainings(MoneyStorage moneyStorage) {
+    RequestRemainings(MoneyStorage moneyStorage) {
         this.moneyStorage = moneyStorage;
     }
 
