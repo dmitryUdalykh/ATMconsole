902bf98e43028780828cb8c1b6403023a6f2a9cd
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-18 23:02:10 +0800

    Minor refactoring2

--- src/AtmStateException.java
+++ src/AtmStateException.java
@@ -1,13 +1,9 @@
 /**
  * Created by Test on 11/06/2016.
  */
-public class AtmStateException extends Exception {
-    // TODO: it's not needed
-    String CauseOfAnError;
+class AtmStateException extends Exception {
 
-    // TODO: "COAE" is invalid name for a method attribute; should be in camel-case; can be named just as a message
-    public AtmStateException(String COAE) {
-        // Let's remove System.out.println and pass "message" to "super" controller as an attribute
-        System.out.println(COAE);
+    AtmStateException(String message) {
+        super(message);
     }
 }
