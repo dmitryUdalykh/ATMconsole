d8f1a3d79b6dcb2716ea51c8bce2c595647d9d93
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-20 12:25:26 +0800

    Minor refactoring2

--- src/CommandType.java
+++ src/CommandType.java
@@ -1,12 +1,10 @@
 /**
  * Created by Test on 11/16/2016.
  */
-//TODO: doesn't have to be public
-public enum CommandType {
+enum CommandType {
     REMAININGS("?"),
     ADD("+"),
     WITHDRAW("-"),
-    EXIT("EXIT"),       // TODO: I don't think EXIT should be on the ATM core commands - real ATMs don't have such a button - let's remove it
     UNDEFINED("");
 
     private final String command;
@@ -15,15 +13,10 @@ public enum CommandType {
         this.command = command;
     }
 
-    // TODO: write a unit test for this method
-    // TODO: why it throws AtmStateException ?
-    public static CommandType getCommandType(String command) throws AtmStateException {
-        // TODO: Let's add a check for "null"
-        // TODO: "CTcheck" - is a local variable - all the local variables. Check here http://javascript.crockford.com/javacodeconventions.pdf
-        // "9 - Naming Conventions". the variable name should start with a lowcase letter and what "CTcheck" means?
-        for (CommandType CTcheck : CommandType.values()) {
-            if (CTcheck.command.equals(command)) {
-                return CTcheck;
+    public static CommandType getCommandType(String command) {
+        for (CommandType commandTypeCheck : CommandType.values()) {
+            if (command != null && commandTypeCheck.command.equals(command)) {
+                return commandTypeCheck;
             }
         }
         return CommandType.UNDEFINED;
