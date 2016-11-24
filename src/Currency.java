60276d0ba7c5bdc3d79ec95ae7c0f6dfeffa537f
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-24 21:39:32 +0800

    Minor refactoring2

--- src/Currency.java
+++ src/Currency.java
@@ -1,7 +1,9 @@
 /**
  * Created by Test on 10/30/2016.
- * TODO: please write a unit test for this class checking that valid currencies are asserted ok and invalid result in throwing AtmStateException
  */
+
+import java.util.Arrays;
+
 enum Currency {
     CHF,
     EUR,
@@ -9,27 +11,20 @@ enum Currency {
     RUR,
     USD;
 
-    //TODO: rename to "getCurrency"; return Currency instead
-    static boolean checkCurrency(String currencyForChecking) throws AtmStateException {
+    static Currency getCurrency(String currencyForChecking) throws AtmStateException {
+        /*
         for (Currency z : Currency.values()) {
-            //TODO: this is risky; of "currencyForChecking" is null then NullPointerException will be thrown
-            // It's better to do it the other way around: z.toString().equals(currencyForChecking) - this way even if currencyForChecking is null
-            // no NPE would be thrown
-            if (currencyForChecking.equals(z.toString())) {
-                // should be just "return"
-                return true;
+            if (z.toString().equals(currencyForChecking)) {
+                return z;
             }
         }
-
         throw new AtmStateException("ILLEGAL CURRENCY TYPE");
+*/
+        //JDK 8 alternative:
 
-        /*
-         * JDK 8 alternative:
-         *
-         * return Arrays.stream(Currency.values())
-         *           .filter(currency -> currency.toString().equals(currencyForChecking))
-         *           .findFirst()
-         *           .orElseThrow(() -> new AtmStateException("ILLEGAL CURRENCY TYPE"));
-         */
+        return Arrays.stream(Currency.values())
+                .filter(currency -> currency.toString().equals(currencyForChecking))
+                .findFirst()
+                .orElseThrow(() -> new AtmStateException("ILLEGAL CURRENCY TYPE"));
     }
 }
