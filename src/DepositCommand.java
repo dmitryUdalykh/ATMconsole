be7621aa4d7ad165d9eb14f3c0e21e6d976da484
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-24 21:28:39 +0800

    Minor refactoring2

--- src/DepositCommand.java
+++ src/DepositCommand.java
@@ -1,6 +1,5 @@
 import java.util.Collections;
 import java.util.Map;
-import java.util.Set;
 
 
 /**
@@ -21,37 +20,13 @@ class DepositCommand implements AtmCommand {
         }
 
         String currencyForDeposit = arguments[0];
-        // TODO: instead of calling 2 lines do "Currency currencyToPut = Currency.getCurrency(currencyForDeposit)"
-        Currency.checkCurrency(currencyForDeposit);
-        Currency currencyToPut = Currency.valueOf(currencyForDeposit);
+        Currency currencyToPut = Currency.getCurrency(currencyForDeposit);
 
-        // TODO: create a utility class names AtmUtils. Create a static method "parseInt" taking 2 parameters - the value to parse and error
-        // message if the value can not be parsed. return integer if the value is ok and throw AtmStateException with the second parameter
-        // if the value is not an integer
-        // Re-use this method wherever int parsing is needed
-        int valueToPut;
-        try {
-            valueToPut = Integer.parseInt(arguments[1]);
-        } catch (NumberFormatException e) {
-            throw new AtmStateException("ILLEGAL TYPING OF VALUE");
-        }
+        int valueToPut = AtmUtils.parseInt(arguments[1], new AtmStateException("ILLEGAL TYPING OF VALUE"));
+        int numberToPut = AtmUtils.parseInt(arguments[2], new AtmStateException("ILLEGAL TYPING OF NUMBER"));
 
-        int numberToPut;
-        try {
-            numberToPut = Integer.parseInt(arguments[2]);
-        } catch (NumberFormatException e) {
-            throw new AtmStateException("ILLEGAL TYPING OF NUMBER");
-        }
-
-        //TODO: let's add a method "assertBanknote(currency, value)" to ExistingBanknotes class and throw AtmStateException from there
-        ExistingBanknotes exBankOne = new ExistingBanknotes();
-        Set<BankNote> bankSet = exBankOne.getExistingBanknotes();
-
-        if (bankSet.contains(new BankNote(currencyToPut, valueToPut))) {
-            moneyStorage.addNotes(currencyToPut, valueToPut, numberToPut);
-            return Collections.singletonMap(new BankNote(currencyToPut, valueToPut), numberToPut);
-        } else {
-            throw new AtmStateException("NOT EXISTING BANKNOTE");
-        }
+        ExistingBanknotes.assertBanknote(currencyToPut, valueToPut);
+        moneyStorage.addNotes(currencyToPut, valueToPut, numberToPut);
+        return Collections.singletonMap(new BankNote(currencyToPut, valueToPut), numberToPut);
     }
 }
