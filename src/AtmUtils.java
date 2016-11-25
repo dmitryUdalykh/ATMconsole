49f5127b82cd53f65c1fc3d92e8909d1c2e967d3
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-25 16:36:19 +0800

    Minor refactoring2

--- src/AtmUtils.java
+++ src/AtmUtils.java
@@ -2,11 +2,16 @@
  * Created by Test on 11/24/2016.
  */
 public class AtmUtils {
-    static int parseInt(String stringToParse, AtmStateException thrownAtmException) throws AtmStateException {
+    static int parseInt(String stringToParse, AtmStateException parseException) throws AtmStateException {
         try {
             return Integer.parseInt(stringToParse);
         } catch (NumberFormatException e) {
-            throw thrownAtmException;
+            throw parseException;
+        }
+    }
+    static void lengthCheck(int lengthValue, AtmStateException lengthException,  String... stringArray) throws AtmStateException{
+        if (stringArray.length != lengthValue) {
+            throw lengthException;
         }
     }
 }
