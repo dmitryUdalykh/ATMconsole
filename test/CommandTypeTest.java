b672f2f2b8ce763ae18562b91ff25d4acb83bf0d
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-25 21:13:08 +0800

    Minor refactoring2

---
+++ test/CommandTypeTest.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Test on 11/25/2016.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CommandTypeTest {
+
+    @Test
+    public void testAtmCommand() throws AtmStateException {
+        CommandType addCheck = CommandType.getCommandType("+");
+        Assert.assertEquals(addCheck, CommandType.ADD);
+
+        CommandType withdrawalCheck = CommandType.getCommandType("-");
+        Assert.assertEquals(withdrawalCheck, CommandType.WITHDRAW);
+
+        CommandType remainCheck = CommandType.getCommandType("?");
+        Assert.assertEquals(remainCheck, CommandType.REMAININGS);
+    }
+
+    @Test(expected = AtmStateException.class)
+    public void testCommandException() throws AtmStateException {
+        CommandType.getCommandType("Abcde");
+    }
+}
