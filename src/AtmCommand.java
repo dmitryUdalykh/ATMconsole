ebc73a7f49d3b6dbc002bc426b2faca7d22a994d
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-25 22:25:06 +0800

    Minor refactoring2

--- src/AtmCommand.java
+++ src/AtmCommand.java
@@ -1,8 +1,9 @@
-import java.util.Map;
-
 /**
  * Created by Test on 10/30/2016.
  */
+
+import java.util.Map;
+
 interface AtmCommand {
     Map<BankNote, Integer> execute(String... arguments) throws AtmStateException;
 }
