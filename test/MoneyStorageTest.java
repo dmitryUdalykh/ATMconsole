8079dc754c7275638c9e687123211f5822860ff7
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-26 16:00:52 +0800

    Minor refactoring2

---
+++ test/MoneyStorageTest.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Test on 11/25/2016.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MoneyStorageTest {
+
+    private MoneyStorage moneyStorage;
+    private Map<BankNote, Integer> storageCheck = new TreeMap<>();
+
+    @Before
+    public void setUp() {
+        moneyStorage = new MoneyStorage();
+    }
+
+    @Test
+    public void moneyStorageTest() throws AtmStateException {
+        moneyStorage.addNotes(Currency.USD, 100, 1);
+        moneyStorage.addNotes(Currency.USD, 100, 2);
+        storageCheck.put(new BankNote(Currency.USD, 100), 3);
+        Assert.assertEquals(storageCheck, moneyStorage.getBanknotes());
+    }
+
+    @Test(expected = AtmStateException.class)
+    public void moneyStorageTest2() throws AtmStateException {
+        moneyStorage.addNotes(Currency.USD, 120, 1);
+    }
+}
